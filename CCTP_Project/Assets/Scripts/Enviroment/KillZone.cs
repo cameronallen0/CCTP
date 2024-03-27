5a2aca6b5bee98e04d40afaf42d7551e475f6567
cameronallen0 <cameron2.allen@live.uwe.ac.uk>
2024-03-27 16:45:44 +0000

    new update
    added more level design
    added killzones
    -killzones respawn the player and reset the movable objects

---
+++ CCTP_Project/Assets/Scripts/Enviroment/KillZone.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillZone : MonoBehaviour
+{
+    public Transform respawnPoint;
+    public TeleportPlayer playerT;
+
+    public string objectTag = "Player";
+    public bool isKill;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag(objectTag))
+        {
+            KillPlayer(other.gameObject);
+        }
+    }
+
+    private void KillPlayer(GameObject player)
+    {
+        player.transform.position = respawnPoint.position;
+        isKill = true;
+        playerT.PlayerFail();
+    }
+}
--- CCTP_Project/Assets/Scripts/Player/TeleportPlayer.cs
+++ CCTP_Project/Assets/Scripts/Player/TeleportPlayer.cs
@@ -10,11 +10,20 @@ public class TeleportPlayer : MonoBehaviour
     private Vector3 objectPos;
     private Vector3 objectCurrentPos;
 
+    private List<ObjectPosition> capturedObjects = new List<ObjectPosition>(); // List to store captured objects and their initial positions
+
     public LayerMask objectLayer;
 
     private GameObject currentObject;
 
     private int teleportRange = 30;
+
+    private KillZone[] allKillZones;
+
+    private void Start()
+    {
+        allKillZones = FindObjectsOfType<KillZone>();
+    }
 
     public void CapturePositions()
     {
@@ -23,10 +32,13 @@ public class TeleportPlayer : MonoBehaviour
         Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
         RaycastHit hit;
         {
-            if(Physics.Raycast(ray, out hit, teleportRange, objectLayer))
+            if (Physics.Raycast(ray, out hit, teleportRange, objectLayer))
             {
                 currentObject = hit.collider.gameObject;
                 objectPos = currentObject.transform.position;
+
+                // Store the captured object and its initial position in the list
+                capturedObjects.Add(new ObjectPosition(currentObject, objectPos));
             }
             else
             {
@@ -44,4 +56,33 @@ public class TeleportPlayer : MonoBehaviour
         transform.position = playerCurrentPos;
         currentObject.transform.position = objectCurrentPos;
     }
+
+    public void PlayerFail()
+    {
+        // Reset all captured objects to their initial positions
+        foreach (ObjectPosition objPos in capturedObjects)
+        {
+            objPos.ResetPosition();
+        }
+
+        capturedObjects.Clear(); // Clear the list after resetting positions
+    }
 }
+
+public class ObjectPosition
+{
+    public GameObject gameObject;
+    public Vector3 initialPosition;
+
+    public ObjectPosition(GameObject obj, Vector3 pos)
+    {
+        gameObject = obj;
+        initialPosition = pos;
+    }
+
+    public void ResetPosition()
+    {
+        gameObject.transform.position = initialPosition;
+    }
+}
+
